c20839707af22c8f7199c1e263845fb2ea49e4b5
salott15 <sarilott@yahoo.com>
2017-03-09 17:30:01 -0500

    initial commit

--- app.js
+++ app.js
@@ -1,1 +1,46 @@
-// your code here!
+$(function() {
+  $('button').mousedown(function(event) {
+  	event.preventDefault();
+  	event.stopPropagation();
+  	var analizer = [];
+  	analizer = $("textarea").val().split(" ")
+  	console.log(analizer)
+  	wordCount(analizer);
+  	lengthAverage(analizer);
+  	uniqueWord(analizer);
+ })
+
+  function wordCount(analizer){
+  	$("dl").removeClass("hidden");
+  	$(".wordCount").text(analizer.length);
+  	}
+
+  function uniqueWord(analizer){
+  	$("d1").removeClass("hidden");
+  	var uniqueWord = [];
+  	uniqueWord.push(analizer[0])
+  	var isUnique = true;
+  	for (var i=0; i<analizer.length; i++){
+  		for(var j=0; j<uniqueWord.length; j++){
+  			if(analizer[i] === uniqueWord[j]){
+  			isUnique = false }
+  		}
+  		if(isUnique){uniqueWord.push(analizer[i])}
+  			isUnique = true;
+  		}
+  		console.log(analizer);
+  		console.log(uniqueWord);
+  		$(".uniqueWord").text(uniqueWord.length);
+  	}
+  function lengthAverage(analizer){
+  	var totalCharacters = 0;
+  	for (var i=0; i<analizer.length; i++){
+  		totalCharacters+=analizer[i].length;
+  	}
+  	var average = totalCharacters / analizer.length;
+  	$(".averageLength").text(average);
+
+  }
+
+  	//$("textarea")
+  })
